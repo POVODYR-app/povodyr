0fbba851a1d301dad0be5da63b4c50ae03f7542e
POVODYR-app <povodyr.app@gmail.com>
2026-09-17 11:02:27 +0300

    Update regex and known dead URLs in buildDigestPortion

--- lib/buildDigestPortion.ts
+++ lib/buildDigestPortion.ts
@@ -1,12 +1,14 @@
 import { personalizeOpportunities, PersonalizedOpportunity } from './personalizeOpportunities'
 
 export const LISTING_TITLE_RE =
-  /актуальний open call та події|актуальні гранти та конкурсні програми|worldwide network open calls|grants database|eu supports ukraine through culture|swiss arts council residencies|selected artists in residence|selected projects|bar laika|presents playback|ghosts\s*-\s*readers|топ світових програм|open calls and opportunities|september \d{4} opportunities|august \d{4}:?\s*open calls|arts opportunities \| arizona|river of stories|time, place & practice|100 emerging artworks/i
-const KNOWN_DEAD_URL_PARTS = [
+export const LISTING_TITLE_RE =
+  /актуальний open call та події|актуальні гранти та конкурсні програми|worldwide network open calls|grants database|eu supports ukraine through culture|swiss arts council residencies|selected artists in residence|selected projects|bar laika|presents playback|ghosts\s*-\s*readers|топ світових програм|open calls and opportunities|september \d{4} opportunities|august \d{4}:?\s*open calls|arts opportunities \| arizona|river of stories|time, place & practice|100 emerging artworks|artist and curatorial fellowships at gasworks|summer sessions:\s*art and technology/iconst KNOWN_DEAD_URL_PARTS = [
   'prohelvetia.ch/en/sundry/residencies',
   'e-flux.com/events/',
   'e-flux.com/readers/',
   'e-flux.com/journal/',
+    'on-the-move.org/news/summer-sessions',
+  'e-flux.com/announcements/6787818/artist-and-curatorial-fellowships-at-gasworks',
 ]
 
 const MS_24H = 24 * 60 * 60 * 1000
